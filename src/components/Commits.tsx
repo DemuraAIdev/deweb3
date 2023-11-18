9d02db34c6886fd38733a44aaf6c11c385806162
DemuraAIdev <cyberaioff@gmail.com>
2023-11-18 13:03:23 +0700

    Update commit time color

--- src/components/Commits.tsx
+++ src/components/Commits.tsx
@@ -13,7 +13,7 @@ export default function Commits() {
         {data?.map((commit) => (
           <li key={commit.sha} className="mb-10 ms-4">
             <div className="absolute -start-1.5 mt-1.5 h-3 w-3 rounded-full border border-white bg-gray-200 dark:border-gray-900 dark:bg-gray-700"></div>
-            <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+            <time className="mb-1 text-sm font-normal leading-none text-gray-500 dark:text-gray-400">
               {commit.time}
             </time>
             <CustomLink href={commit.url} showIcon={false}>
