73304ff98d59d2f6904d20764012156d0448d229
DemuraAIdev <cyberaioff@gmail.com>
2023-11-18 13:17:05 +0700

    Fix heading level in about layout

--- src/app/[lang]/about/page.tsx
+++ src/app/[lang]/about/page.tsx
@@ -2,7 +2,6 @@ import { MDXRender } from '@/lib/MDXRender'
 import { Authors, allAuthors } from 'contentlayer/generated'
 const author = allAuthors.find((p) => p.slug === 'default') as Authors
 
-
 export default function Home() {
   return <MDXRender code={author.body.code} />
 }
