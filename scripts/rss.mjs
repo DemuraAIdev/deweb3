1e038c569931186a610177262115538a2e0a255b
DemuraAIdev <cyberaioff@gmail.com>
2023-12-18 09:33:01 +0700

    Fix links in feed.xml and update lastBuildDate

--- scripts/rss.mjs
+++ scripts/rss.mjs
@@ -32,9 +32,9 @@ var escape = (es) => replace.call(es, ca, pe)
 
 const generateRssItem = (config, post) => `
   <item>
-    <guid>${config.siteURL}/blog/${post.slug}</guid>
+    <guid>${config.siteURL}blog/${post.slug}</guid>
     <title>${escape(post.title)}</title>
-    <link>${config.siteURL}/blog/${post.slug}</link>
+    <link>${config.siteURL}blog/${post.slug}</link>
     ${post.summary && `<description>${escape(post.summary)}</description>`}
     <pubDate>${new Date(post.date).toUTCString()}</pubDate>
     <author>${config.email} (${config.author})</author>
@@ -46,7 +46,7 @@ const generateRss = (config, posts, page = 'feed.xml') => `
   <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
     <channel>
       <title>${escape(config.title)}</title>
-      <link>${config.siteURL}/blog</link>
+      <link>${config.siteURL}blog</link>
       <description>${escape(config.description)}</description>
       <language>${config.language}</language>
       <managingEditor>${config.email} (${config.author})</managingEditor>
@@ -64,18 +64,6 @@ async function generateRSS(config, allBlogs, page = 'feed.xml') {
   if (publishPosts.length > 0) {
     const rss = generateRss(config, sortPosts(publishPosts))
     writeFileSync(`./public/${page}`, rss)
-  }
-
-  if (publishPosts.length > 0) {
-    for (const tag of Object.keys(tagData)) {
-      const filteredPosts = allBlogs.filter((post) =>
-        post.tags.map((t) => slugger.slug(t)).includes(tag)
-      )
-      const rss = generateRss(config, filteredPosts, `tags/${tag}/${page}`)
-      const rssPath = path.join('public', 'tags', tag)
-      mkdirSync(rssPath, { recursive: true })
-      writeFileSync(path.join(rssPath, page), rss)
-    }
   }
 }
 
