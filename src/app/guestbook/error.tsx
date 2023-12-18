495e9f5239ffc2aafa60aaa1910fbcbfbc6c5943
DemuraAIdev <cyberaioff@gmail.com>
2023-12-18 17:15:49 +0700

    Add Error component to handle errors in the guestbook

--- src/app/guestbook/error.tsx
+++ src/app/guestbook/error.tsx
@@ -1,15 +1,21 @@
 'use client'
 
-export default function GlobalError({
+import { useEffect } from 'react'
+
+export default function Error({
   error,
   reset,
 }: {
   error: Error & { digest?: string }
   reset: () => void
 }) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error)
+  }, [error])
+
   return (
     <div className="-mt-24 flex min-h-screen flex-col items-center justify-center py-2 text-center">
-      <h1 className="text-9xl font-bold">500?</h1>
       <h2 className="text-6xl font-medium">Something went wrong!</h2>
       <p className="text-2xl font-light">The page you are looking for does not exist.</p>
       <button onClick={() => reset()}>Try again</button>
