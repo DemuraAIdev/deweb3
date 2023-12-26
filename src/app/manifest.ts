aa219112cbfea97499e099f8bf53780ce2d62608
DemuraAIdev <cyberaioff@gmail.com>
2023-12-26 16:38:54 +0700

    Add new logo files

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -12,6 +12,11 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#fff',
     icons: [
       {
+        src: '/favicon.ico',
+        sizes: 'any',
+        type: 'image/x-icon',
+      },
+      {
         src: '/icons/android-chrome-192x192.png',
         sizes: '192x192',
         type: 'image/png',
