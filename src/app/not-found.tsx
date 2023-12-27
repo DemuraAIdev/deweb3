aad8f33c4614f9d73634dd9202093c10f7d13547
DemuraAIdev <cyberaioff@gmail.com>
2023-12-27 17:11:22 +0700

    Update error and not-found pages

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,9 @@ import Link from 'next/link'
 export default function NotFound() {
   return (
     <div className="-mt-24 flex min-h-screen flex-col items-center justify-center py-2 text-center">
-      <h1 className="text-9xl font-bold">404</h1>
+      <h1 className="text-9xl font-bold">
+        ~({'>'}_{'<'}。)＼
+      </h1>
       <h2 className="text-6xl font-medium">Page Not Found</h2>
       <p className="text-2xl font-light">The page you are looking for does not exist.</p>
       <Link href="/" className="text-blue-500 hover:underline">
--- src/types/Component.ts
+++ src/types/Component.ts
@@ -1,9 +1,8 @@
-import { AnchorHTMLAttributes } from "react";
+import { AnchorHTMLAttributes } from 'react'
 import type { LinkProps } from 'next/link'
 
-
 export interface CustomLinkType extends LinkProps, AnchorHTMLAttributes<HTMLAnchorElement> {
-    href: string;
-    showIcon?: boolean;
-    [key: string]: unknown;
+  href: string
+  showIcon?: boolean
+  [key: string]: unknown
 }
