3241ebe55dabbfa76c2ce215ac029fb0464b8da7
DemuraAIdev <cyberaioff@gmail.com>
2023-12-31 10:38:01 +0700

    Refactor NavLink component styles

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -14,19 +14,20 @@ export default function NavLink({
   className?: string
 }) {
   const isActive = usePathname() === href
+  const baseClassName =
+    'hidden rounded-lg p-1 transition-all hover:bg-gray-200 dark:hover:bg-gray-800 sm:block sm:px-3 sm:py-2 md:inline-block'
+
+  const activeStyles =
+    'font-semibold text-black hover:text-black dark:text-white dark:hover:text-white'
+  const inactiveStyles =
+    'font-medium text-gray-500 hover:text-black dark:text-gray-400 dark:hover:text-white'
+
+  const combinedClassName = `${baseClassName} ${isActive ? activeStyles : inactiveStyles} ${
+    className || ''
+  }`
 
   return (
-    <CustomLink
-      key={text}
-      href={href}
-      className={
-        isActive
-          ? 'hidden rounded-lg p-1 font-semibold text-black transition-all hover:bg-gray-200 hover:text-black  dark:text-white dark:hover:bg-gray-800 dark:hover:text-white sm:block sm:px-3 sm:py-2 md:inline-block ' +
-            className
-          : 'hidden rounded-lg p-1 font-medium text-gray-500 transition-all hover:bg-gray-200 hover:text-black  dark:text-gray-400 dark:hover:bg-gray-800 dark:hover:text-white sm:block sm:px-3 sm:py-2 md:inline-block ' +
-            className
-      }
-    >
+    <CustomLink key={text} href={href} className={combinedClassName}>
       {text}
     </CustomLink>
   )
