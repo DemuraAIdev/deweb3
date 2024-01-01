95b7c7e7b387578521836452b8eb9e63801f3380
DemuraAIdev <cyberaioff@gmail.com>
2024-01-01 20:02:34 +0700

    Update Wakatime API URLs

--- src/lib/wakatime.ts
+++ src/lib/wakatime.ts
@@ -2,35 +2,50 @@ import * as Wakatime from '@/types/Wakatime'
 
 export const WeekleCodingActivity = async () => {
   const res = await fetch(
-    'https://wakatime.com/share/@DemuraAIdev/3fdbe5d5-2d24-431d-944e-563b37075f84.json'
+    'https://wakatime.com/share/@DemuraAIdev/eecd8b67-97fa-4618-9b90-13bc78ab104c.json',
+    {
+      next: { revalidate: 86400 },
+    }
   )
   return res.json() as Promise<Wakatime.WeeklyCodingActivity>
 }
 
 export const Languages = async () => {
   const res = await fetch(
-    'https://wakatime.com/share/@DemuraAIdev/bbf2a61a-2021-4b88-91bc-9458938c73ce.json'
+    'https://wakatime.com/share/@DemuraAIdev/3201cfbb-f2db-4b95-b9be-a624c91aacfd.json',
+    {
+      next: { revalidate: 86400 },
+    }
   )
   return res.json() as Promise<Wakatime.WeeklyCodingLanguanges>
 }
 
 export const OpratingSystems = async () => {
   const res = await fetch(
-    'https://wakatime.com/share/@DemuraAIdev/bef4f887-9064-4b8e-9731-c541acb5ff44.json'
+    'https://wakatime.com/share/@DemuraAIdev/733a58e0-46a6-4e41-859a-8a35c727db88.json',
+    {
+      next: { revalidate: 86400 },
+    }
   )
   return res.json() as Promise<Wakatime.WeeklyOperatingSystems>
 }
 
 export const Editors = async () => {
   const res = await fetch(
-    'https://wakatime.com/share/@DemuraAIdev/31c3563d-6d6e-4383-871b-a3387610a8b7.json'
+    'https://wakatime.com/share/@DemuraAIdev/6005593a-737a-4a1f-ab1c-a617ca203c7e.json',
+    {
+      next: { revalidate: 86400 },
+    }
   )
   return res.json() as Promise<Wakatime.WeeklyCodeEditor>
 }
 
 export const Categories = async () => {
   const res = await fetch(
-    'https://wakatime.com/share/@DemuraAIdev/709b9eb3-0736-4f26-897b-65eafea70bda.json'
+    'https://wakatime.com/share/@DemuraAIdev/c77d6d2f-644c-4ac1-825c-82f56eba6353.json',
+    {
+      next: { revalidate: 86400 },
+    }
   )
   return res.json() as Promise<Wakatime.WeeklyCategories>
 }
