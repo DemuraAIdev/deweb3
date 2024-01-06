fda50fbeb7f86ac3cc64fe0e74044ee1e4ebc48c
DemuraAIdev <cyberaioff@gmail.com>
2024-01-06 18:29:34 +0700

    Update dependencies and import statements

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,40 +1,11 @@
-// import { PrismaClient } from '@prisma/client/edge'
-// import { withAccelerate } from '@prisma/extension-accelerate'
-
-// // Learn more about instantiating PrismaClient in Next.js here: https://www.prisma.io/docs/data-platform/accelerate/getting-started
-
-// const prismaClientSingleton = () => {
-//   return new PrismaClient().$extends(withAccelerate())
-// }
-
-// type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
-
-// const globalForPrisma = globalThis as unknown as {
-//   prisma: PrismaClientSingleton | undefined
-// }
-
-// const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
-
-// export default prisma
-
-// if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
-
-// ts-ignore 7017 is used to ignore the error that the global object is not
-// defined in the global scope. This is because the global object is only
-// defined in the global scope in Node.js and not in the browser.
-
 import { PrismaClient } from '@prisma/client'
-
-// PrismaClient is attached to the `global` object in development to prevent
-// exhausting your database connection limit.
-//
-// Learn more:
-// https://pris.ly/d/help/next-js-best-practices
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-export const prisma = globalForPrisma.prisma || new PrismaClient()
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ['query'],
+  })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
-
-export default prisma
