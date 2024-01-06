2afa5e4257465609058a681b8179092d709099ec
DemuraAIdev <cyberaioff@gmail.com>
2024-01-06 21:36:21 +0700

    Add refresh functionality and update middleware configuration

--- src/app/maintenance/page.tsx
+++ src/app/maintenance/page.tsx
@@ -1,5 +1,7 @@
-import Link from '@/components/Link'
+'use client'
+import { useRouter } from 'next/navigation'
 export default function Page() {
+  const { refresh } = useRouter()
   return (
     <div className="-mt-24 flex min-h-screen flex-col items-center justify-center py-2 text-center">
       <h1 className="text-5xl font-bold sm:text-9xl">
@@ -7,9 +9,9 @@ export default function Page() {
       </h1>
       <h2 className="text-6xl font-medium">Maintenance</h2>
       <p className="text-2xl font-light">Can you wait for a year lol?.</p>
-      <Link href="/" className="text-blue-500 hover:underline">
-        Go back home
-      </Link>
+      <button onClick={() => refresh()} className="text-blue-500 hover:underline">
+        Try again
+      </button>
     </div>
   )
 }
