d43c2624c8c386855ae1956db66258888bb12819
DemuraAIdev <cyberaioff@gmail.com>
2024-01-06 09:45:32 +0700

    Refactor ProjectCard component structure

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -11,20 +11,20 @@ interface CardProp {
 
 export default function ProjectCard({ title, description, imgSrc, href, discontinued }: CardProp) {
   return (
-    <div className="w-full p-4 md:w-1/2">
-      {/* <div className="flex h-full transform flex-col justify-between rounded-lg border-2 border-dotted border-gray-500 bg-white p-5  transition duration-500 hover:scale-[1.03]  dark:bg-[#111010] dark:hover:bg-gray-800"> */}
-      <article
-        className={
-          'hover:shadow-secondary group h-full overflow-hidden rounded-md border-2 border-dotted border-gray-500 p-5 transition-transform duration-500' +
-          (discontinued ? ' opacity-50 hover:border-red-600 ' : ' hover:scale-[1.03]')
-        }
-      >
-        <Link
-          href={href!}
-          showIcon={false}
-          aria-label={`Link to ${title}`}
-          data-umami-event="Project links"
-          data-umami-event-project={title}
+    <Link
+      href={href!}
+      showIcon={false}
+      aria-label={`Link to ${title}`}
+      data-umami-event="Project links"
+      data-umami-event-project={title}
+    >
+      <div className="w-full p-4 md:w-1/2">
+        {/* <div className="flex h-full transform flex-col justify-between rounded-lg border-2 border-dotted border-gray-500 bg-white p-5  transition duration-500 hover:scale-[1.03]  dark:bg-[#111010] dark:hover:bg-gray-800"> */}
+        <article
+          className={
+            'hover:shadow-secondary group h-full overflow-hidden rounded-md border-2 border-dotted border-gray-500 p-5 transition-transform duration-500' +
+            (discontinued ? ' opacity-50 hover:border-red-600 ' : ' hover:scale-[1.03]')
+          }
         >
           <figure className="relative aspect-video overflow-hidden">
             {imgSrc ? (
@@ -61,8 +61,8 @@ export default function ProjectCard({ title, description, imgSrc, href, disconti
           <h2 className="mb-2 mt-3 text-2xl font-bold leading-8 tracking-tight">{title}</h2>
           <p className="prose mb-3 max-w-none text-gray-500 dark:text-gray-400">{description}</p>
           {/* </div> */}
-        </Link>
-      </article>
-    </div>
+        </article>
+      </div>
+    </Link>
   )
 }
