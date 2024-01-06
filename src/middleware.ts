2352486cf5daf4df59002de0ff00149c103a5eae
DemuraAIdev <cyberaioff@gmail.com>
2024-01-06 21:15:38 +0700

    Add @vercel/edge-config package and create middleware for maintenance mode

---
+++ src/middleware.ts
@@ -0,0 +1,19 @@
+import { NextRequest, NextResponse } from 'next/server'
+import { get } from '@vercel/edge-config'
+
+export const config = {
+  matcher: '/big-promo',
+}
+
+export async function middleware(req: NextRequest) {
+  // Check Edge Config to see if the maintenance page should be shown
+  const isInMaintenanceMode = await get('isInMaintenanceMode')
+
+  // If in maintenance mode, point the url pathname to the maintenance page
+  if (isInMaintenanceMode) {
+    req.nextUrl.pathname = `/maintenance`
+
+    // Rewrite to the url
+    return NextResponse.rewrite(req.nextUrl)
+  }
+}
