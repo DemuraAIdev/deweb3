26ed11f9a8858274522223dee60c8c138c077a62
DemuraAIdev <cyberaioff@gmail.com>
2024-05-13 21:21:19 +0700

    chore: Update anime Card component with new link URL

---
+++ src/app/api/animelist/getinfo/[id]/route.ts
@@ -0,0 +1,15 @@
+import { getAnime } from '@/lib/myanimelist'
+import { NextRequest, NextResponse } from 'next/server'
+// export const runtime = 'edge'
+import type { UserWatchAnime } from '@/lib/myanimelist'
+
+export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
+  const url_param_status = params.id
+
+  if (url_param_status) {
+    const response: UserWatchAnime[] = await getAnime(url_param_status)
+    return NextResponse.json(response)
+  }
+
+  return NextResponse.json({}) // Fix: Replace resresponseponse with a valid response object
+}
