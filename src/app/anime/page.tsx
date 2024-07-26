74c87f89633cfd646cc2fc65f41b8f2549be4487
DemuraAIdev <cyberaioff@gmail.com>
2024-07-26 23:38:38 +0700

    chore: Clean up code and remove unused imports in middleware.ts

--- src/app/anime/page.tsx
+++ src/app/anime/page.tsx
@@ -6,7 +6,7 @@ export const metadata = genPageMetadata({ title: 'Anime' })
 export default function Anime() {
   return (
     <div>
-      <div className=" space-y-2 pb-8 pt-6 md:space-y-5">
+      <div className="space-y-2 pb-8 pt-6 md:space-y-5">
         <h1 className="text-4xl font-bold  tracking-tight text-gray-900 dark:text-gray-100">
           Anime Watch List <IoIosStats className="inline-block" />
         </h1>
@@ -14,7 +14,7 @@ export default function Anime() {
           This data powered By MyAnimeList
         </p>
       </div>
-      <div className="w-full md:w-1/2">
+      <div className="container py-12">
         <Link href={'/anime/completed'} showIcon={false} aria-label={`Link to /anime/completed`}>
           <div className="flex h-full transform flex-col justify-between rounded-lg border-2 border-dotted border-gray-500 bg-white p-2  transition duration-500 hover:scale-[1.03]  dark:bg-[#111010] dark:hover:bg-gray-800">
             <div className="p-6">
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -7,16 +7,16 @@ export const metadata = genPageMetadata({ title: 'Projects' })
 export default async function Projects() {
   return (
     <>
-      <div className="mb-4 space-y-2 pt-6 md:space-y-5">
-        <h1 className="text-4xl font-bold tracking-tight text-gray-900 dark:text-gray-100 ">
+      <div className="container mx-auto mb-4 space-y-2 pt-6 md:space-y-5">
+        <h1 className="text-4xl font-bold tracking-tight text-gray-900 dark:text-gray-100">
           Project <FaGear className="inline-block hover:animate-spin" />
         </h1>
-        <p className=" leading-7 text-gray-500 dark:text-gray-400">
+        <p className="leading-7 text-gray-500 dark:text-gray-400">
           My project that I have done. I hope you like it. This project showcases my skills in web
           development and design. I have also contributed to open source projects .
         </p>
       </div>
-      <div className="container py-12">
+      <div className="container mx-auto py-12">
         <div className="-m-4 flex flex-wrap">
           {projectsData.map((d) => (
             <Card
